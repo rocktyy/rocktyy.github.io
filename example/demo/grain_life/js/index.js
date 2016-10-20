93ed1bb5e0c69191de521b09dc91ff39205be2ae
tianyuyan <tianyuyan@163.com>
2016-10-20 20:41:41 +0800

    Site updated: 2016-10-20 20:41:40

--- example/demo/grain_life/js/index.js
+++ example/demo/grain_life/js/index.js
@@ -157,7 +157,7 @@ define(function (require, exports, module) {
                 var start = new Date().getTime();
                 while (true) if (new Date().getTime() - start > n) break;
             }  
-            sleep(600);
+            sleep(100);
         })
 
         setInterval(function () {
